76b5c22f0dd152f425c3e4659b98ad2e8dce7341
Adrien Perrin <adrien.perrin@nersc.no>
2020-02-21 12:24:43 +0000

    add base and geospatial metadata handlers

---
+++ metanorm/handlers.py
@@ -0,0 +1,40 @@
+"""
+Handler class for normalizers. It is possible to create a custom handler which uses a custom list
+of normalizers: just inherit from MetadataHandler and override the NORMALIZERS class attribute.
+"""
+import logging
+
+import metanorm.normalizers as normalizers
+
+LOGGER = logging.getLogger(__name__)
+
+
+class MetadataHandler():
+    """Base handler"""
+    # This list should be ordered by decreasing priority, and
+    # THE LAST NORMALISER MUST INHERIT FROM BaseDefaultMetadataNormalizer
+    NORMALIZERS = []
+
+    def __init__(self, parameter_names):
+        """Builds a chain of normalizers for the given parameter names"""
+
+        self._chain = last_normalizer = self.NORMALIZERS[0](parameter_names)
+
+        for normalizer_class in self.NORMALIZERS[1:]:
+            current_normalizer = normalizer_class(parameter_names)
+            last_normalizer.next = current_normalizer
+            last_normalizer = current_normalizer
+
+    def get_parameters(self, raw_attributes):
+        """Loop through normalizers to try and get a value for each parameter"""
+        return self._chain.normalize(raw_attributes)
+
+
+class GeospatialMetadataHandler(MetadataHandler):
+    """Geospatial metadata handler"""
+
+    NORMALIZERS = [
+        normalizers.ACDDMetadataNormalizer,
+        normalizers.GeoSpatialWellKnownMetadataNormalizer,
+        normalizers.GeoSpatialDefaultMetadataNormalizer
+    ]
