5aafc440dbc7c395d72d8a0f706e874734e4c84c
Adrien Perrin <adrien.perrin@nersc.no>
2020-03-20 11:29:22 +0100

    add tests for the copernicus metadata normalizer

---
+++ tests/normalizers/test_sentinel_safe.py
@@ -0,0 +1,280 @@
+"""Tests for the ACDD metadata normalizer"""
+import unittest
+from collections import OrderedDict
+from datetime import datetime
+
+from dateutil.tz import tzutc
+from django.contrib.gis.geos.geometry import GEOSGeometry
+
+import metanorm.normalizers as normalizers
+
+
+class SentinelSAFEMetadataNormalizerTestCase(unittest.TestCase):
+    """Tests for the ACDD attributes normalizer"""
+
+    def setUp(self):
+        self.normalizer = normalizers.SentinelSAFEMetadataNormalizer([])
+
+    def test_entry_title(self):
+        """entry_title from SentinelSAFEMetadataNormalizer"""
+        attributes = {'Identifier': 'title_value'}
+        self.assertEqual(self.normalizer.get_entry_title(attributes), 'title_value')
+
+    def test_entry_title_missing_attribute(self):
+        """Parameter method must return None if the attribute is missing"""
+        self.assertEqual(self.normalizer.get_entry_title({}), None)
+
+    def test_summary(self):
+        """summary from SentinelSAFEMetadataNormalizer"""
+        attributes = {
+            'Date': '2018-04-18T01:02:03Z',
+            'Instrument': 'instrument_value',
+            'Mode': 'mode_value',
+            'Satellite': 'satellite_value',
+            'Size': 'size_value'
+        }
+        self.assertEqual(
+            self.normalizer.get_summary(attributes),
+            'Date: 2018-04-18T01:02:03Z, Instrument: instrument_value, Mode: mode_value, ' +
+            'Satellite: satellite_value, Size: size_value')
+
+    def test_summary_missing_attribute(self):
+        """Parameter method must return None if the attribute is missing"""
+        self.assertEqual(self.normalizer.get_summary({}), None)
+
+    def test_time_coverage_start(self):
+        """time_coverage_start from SentinelSAFEMetadataNormalizer"""
+        self.assertEqual(
+            self.normalizer.get_time_coverage_start({'Sensing start': "20200101T000001"}),
+            datetime(year=2020, month=1, day=1, hour=0, minute=0, second=1))
+        self.assertEqual(
+            self.normalizer.get_time_coverage_start({'Sensing start': "20200101T000001Z"}),
+            datetime(year=2020, month=1, day=1, hour=0, minute=0, second=1, tzinfo=tzutc()))
+        self.assertEqual(
+            self.normalizer.get_time_coverage_start({'Sensing start': "2020-01-01T00:00:01"}),
+            datetime(year=2020, month=1, day=1, hour=0, minute=0, second=1))
+        self.assertEqual(
+            self.normalizer.get_time_coverage_start({'Sensing start': "2020-01-01T00:00:01Z"}),
+            datetime(year=2020, month=1, day=1, hour=0, minute=0, second=1, tzinfo=tzutc()))
+
+    def test_time_coverage_start_missing_attribute(self):
+        """Parameter method must return None if the attribute is missing"""
+        self.assertEqual(self.normalizer.get_time_coverage_start({}), None)
+
+    def test_time_coverage_end(self):
+        """time_coverage_end from SentinelSAFEMetadataNormalizer"""
+        self.assertEqual(
+            self.normalizer.get_time_coverage_end({'Sensing stop': "20200101T000559"}),
+            datetime(year=2020, month=1, day=1, hour=0, minute=5, second=59))
+        self.assertEqual(
+            self.normalizer.get_time_coverage_end({'Sensing stop': "20200101T000559Z"}),
+            datetime(year=2020, month=1, day=1, hour=0, minute=5, second=59, tzinfo=tzutc()))
+        self.assertEqual(
+            self.normalizer.get_time_coverage_end({'Sensing stop': "2020-01-01T00:05:59"}),
+            datetime(year=2020, month=1, day=1, hour=0, minute=5, second=59))
+        self.assertEqual(
+            self.normalizer.get_time_coverage_end({'Sensing stop': "2020-01-01T00:05:59Z"}),
+            datetime(year=2020, month=1, day=1, hour=0, minute=5, second=59, tzinfo=tzutc()))
+
+    def test_time_coverage_end_missing_attribute(self):
+        """Parameter method must return None if the attribute is missing"""
+        self.assertEqual(self.normalizer.get_time_coverage_end({}), None)
+
+    def test_gcmd_platform(self):
+        """gcmd_platform from SentinelSAFEMetadataNormalizer"""
+        attributes = {'Satellite name': 'SENTINEL-1', 'Satellite number': 'B'}
+
+        self.assertEqual(
+            self.normalizer.get_platform(attributes),
+            OrderedDict([('Category', 'Earth Observation Satellites'),
+                         ('Series_Entity', 'SENTINEL-1'),
+                         ('Short_Name', 'SENTINEL-1B'),
+                         ('Long_Name', 'SENTINEL-1B')])
+        )
+
+    def test_non_gcmd_platform(self):
+        """Non-GCMD platform from SentinelSAFEMetadataNormalizer"""
+        attributes = {'Satellite name': 'TEST', 'Satellite number': 'A'}
+
+        self.assertEqual(
+            self.normalizer.get_platform(attributes),
+            OrderedDict([
+                ('Category', 'Unknown'),
+                ('Series_Entity', 'Unknown'),
+                ('Short_Name', 'TESTA'),
+                ('Long_Name', 'TESTA')
+            ])
+        )
+
+    def test_non_gcmd_platform_long_name(self):
+        """
+        Non-GCMD platform from SentinelSAFEMetadataNormalizer, with a name longer than 250 characters
+        """
+        attributes = {
+            'Satellite name': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ei' +
+                              'usmod tempor incididunt ut labore et dolore magna aliqua. Bibendum' +
+                              ' neque egest as congue quisque egestas diam in. Eget magna ferment' +
+                              'um iaculis eu non diam phasellus vestibulum lorvgem. Tempor commodo',
+            'Satellite number': 'A'
+        }
+
+        self.assertEqual(
+            self.normalizer.get_platform(attributes),
+            OrderedDict([
+                ('Category', 'Unknown'),
+                ('Series_Entity', 'Unknown'),
+                ('Short_Name', attributes['Satellite name'][:100]),
+                ('Long_Name', attributes['Satellite name'][:250])
+            ])
+        )
+
+    def test_platform_missing_attribute(self):
+        """Parameter method must return None if the attribute is missing"""
+        self.assertEqual(self.normalizer.get_platform({}), None)
+
+    def test_gcmd_instrument(self):
+        """GCMD instrument from SentinelSAFEMetadataNormalizer"""
+        attributes = {'Instrument': 'SAR-C SAR'}
+
+        self.assertEqual(
+            self.normalizer.get_instrument(attributes),
+            OrderedDict([('Category', 'Earth Remote Sensing Instruments'),
+                         ('Class', 'Active Remote Sensing'),
+                         ('Type', 'Imaging Radars'),
+                         ('Subtype', ''),
+                         ('Short_Name', 'SAR'),
+                         ('Long_Name', 'Synthetic Aperture Radar')])
+        )
+
+    def test_non_gcmd_instrument(self):
+        """Non-GCMD instrument from SentinelSAFEMetadataNormalizer"""
+        attributes = {'Instrument': 'TEST'}
+
+        self.assertEqual(
+            self.normalizer.get_instrument(attributes),
+            OrderedDict([
+                ('Category', 'Unknown'),
+                ('Class', 'Unknown'),
+                ('Type', 'Unknown'),
+                ('Subtype', 'Unknown'),
+                ('Short_Name', 'TEST'),
+                ('Long_Name', 'TEST')
+            ])
+        )
+
+    def test_non_gcmd_instrument_long_name(self):
+        """
+        Non-GCMD instrument from SentinelSAFEMetadataNormalizer, with a name longer than 200 characters
+        """
+
+        attributes = {
+            'Instrument':
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
+                'tempor incididunt ut labore et dolore magna aliqua. Bibendum neque egest' +
+                'as congue quisque egestas diam in. Eget magna fermentum iaculis eu non d' +
+                'iam phasellus vestibulum lorvgem. Tempor commodo.'
+        }
+
+        self.assertEqual(
+            self.normalizer.get_instrument(attributes),
+            OrderedDict([
+                ('Category', 'Unknown'),
+                ('Class', 'Unknown'),
+                ('Type', 'Unknown'),
+                ('Subtype', 'Unknown'),
+                ('Short_Name', attributes['Instrument'][:60]),
+                ('Long_Name', attributes['Instrument'][:200])
+            ])
+        )
+
+    def test_instrument_missing_attribute(self):
+        """Parameter method must return None if the attribute is missing"""
+        self.assertEqual(self.normalizer.get_instrument({}), None)
+
+    def test_wkt_bounds_location_geometry(self):
+        """location_geometry from SentinelSAFEMetadataNormalizer"""
+
+        attributes = {
+            'JTS footprint': (
+                'MULTIPOLYGON(((' +
+                '-29.04 61.31,' +
+                '-18.32 59.66,' +
+                '-20.25 51.06,' +
+                '-38.97 55.12,' +
+                '-29.04 61.31)))'),
+            'geospatial_bounds_crs': 'EPSG:4326'
+        }
+        expected_geometry = GEOSGeometry(
+            ('MULTIPOLYGON(((' +
+             '-29.04 61.31,' +
+             '-18.32 59.66,' +
+             '-20.25 51.06,' +
+             '-38.97 55.12,' +
+             '-29.04 61.31)))'),
+            srid=4326)
+
+        self.assertTrue(self.normalizer.get_location_geometry(attributes).equals(expected_geometry))
+
+    def test_location_geometry_missing_attribute(self):
+        """Parameter method must return None if the attribute is missing"""
+        self.assertEqual(self.normalizer.get_location_geometry({}), None)
+
+    def test_gcmd_provider(self):
+        """GCMD provider from SentinelSAFEMetadataNormalizer"""
+        attributes = {'Operator': 'European Space Agency'}
+
+        self.assertEqual(
+            self.normalizer.get_provider(attributes),
+            OrderedDict([('Bucket_Level0', 'MULTINATIONAL ORGANIZATIONS'),
+                         ('Bucket_Level1', ''),
+                         ('Bucket_Level2', ''),
+                         ('Bucket_Level3', ''),
+                         ('Short_Name', 'ESA/EO'),
+                         ('Long_Name', 'Observing the Earth, European Space Agency'),
+                         ('Data_Center_URL', 'http://www.esa.int/esaEO/')])
+        )
+
+    def test_non_gcmd_provider(self):
+        """Non-GCMD provider from SentinelSAFEMetadataNormalizer"""
+        attributes = {'Operator': 'TEST'}
+
+        self.assertEqual(
+            self.normalizer.get_provider(attributes),
+            OrderedDict([
+                ('Bucket_Level0', 'Unknown'),
+                ('Bucket_Level1', 'Unknown'),
+                ('Bucket_Level2', 'Unknown'),
+                ('Bucket_Level3', 'Unknown'),
+                ('Short_Name', 'TEST'),
+                ('Long_Name', 'TEST'),
+                ('Data_Center_URL', 'Unknown')
+            ])
+        )
+
+    def test_non_gcmd_provider_long_name(self):
+        """Non-GCMD provider from SentinelSAFEMetadataNormalizer, with a name longer than 250 characters"""
+
+        attributes = {
+            'Operator':
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
+                'tempor incididunt ut labore et dolore magna aliqua. Bibendum neque egest' +
+                'as congue quisque egestas diam in. Eget magna fermentum iaculis eu non d' +
+                'iam phasellus vestibulum lorvgem. Tempor commodo.'
+        }
+
+        self.assertEqual(
+            self.normalizer.get_provider(attributes),
+            OrderedDict([
+                ('Bucket_Level0', 'Unknown'),
+                ('Bucket_Level1', 'Unknown'),
+                ('Bucket_Level2', 'Unknown'),
+                ('Bucket_Level3', 'Unknown'),
+                ('Short_Name', attributes['Operator'][:50]),
+                ('Long_Name', attributes['Operator'][:250]),
+                ('Data_Center_URL', 'Unknown')
+            ])
+        )
+
+    def test_provider_missing_attribute(self):
+        """Parameter method must return None if the attribute is missing"""
+        self.assertEqual(self.normalizer.get_provider({}), None)
