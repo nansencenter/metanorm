0fa468d6e5ee2aac45f90015d43ddea35628ab7a
arash <62345618+aanersc@users.noreply.github.com>
2020-07-15 15:15:56 +0200

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="metanorm",
-    version="1.1.0",
+    version="1.1.1",
     author="Adrien Perrin",
     author_email="adrien.perrin@nersc.no",
     description="Normalizing tool for Geospatial metadata",
