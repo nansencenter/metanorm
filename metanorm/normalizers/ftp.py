7750b4f3dd6d91b1d5f7bc6b26b63b6a847a2744
aanersc <arash.azamifard@nersc.no>
2020-09-01 07:43:51 +0200

    #16 converting the get_instrument

--- metanorm/normalizers/ftp.py
+++ metanorm/normalizers/ftp.py
@@ -18,36 +18,42 @@ LOGGER.addHandler(logging.NullHandler())
 
 class FTPMetadataNormalizer(BaseMetadataNormalizer):
     """ Normalizer for hardcoding information for the ftp-derived cases """
-    metadata_name = 'ftp_domain_name'
     # Since the addressing is different in the various ftp resources, below dictionary is used
     # to find the correct part of the path based on each ftp addressing criteria.
     # This dictionary contains all the folder names regardless of their hierarchy
     # in the very fto source. A regext with "compile" and "findall" is used to find the proper name
     # form it.
-    domain_map = {"anon-ftp.ceda.ac.uk": r'esacci|aerosol|biomass|cloud|esacci_terms_and_conditions\.txt|fire|ghg|glaciers|ice_sheets_antarctica|ice_sheets_greenland|lakes|land_cover|ocean_colour|ozone|permafrost|sea_ice|sea_level|sea_state|sea_surface_salinity|snow|soil_moisture|sst',
-                  "ftp.remss.com": r'amsr2|amsre|amsrj|ascat|ccmp|gmi|msu|nscat|qscat|seawinds|smap|ssmi|sst|support|TC-winds|tc_wakes|tmi|vapor|water_cycle|web\.config|welcome\.txt|wind|windsat',
-                  "ftp.gportal.jaxa.jp": r'JERS-1|ADEOS-2|MOS-1b|SLATS|CIRC|MOS-1|AQUA|GCOM-C|GCOM-W|GSMaP|ADEOS|GPM|GPMConstellation|TRMM|TRMM_GPMFormat|GCOM-W\.AMSR2|L3\.PRC_10|L3\.SIC_10|L2\.TPW|L3\.TPW_25|L3\.CLW_10|L3\.SSW_10|L3\.TB36GHz_25|L3\.TB6GHz_10|L2\.SIC|L3\.TB89GHz_25|L2\.SSW|L3\.TB36GHz_10|L2\.PRC|L3\.SND_25|L3\.SMC_10|L3\.TB23GHz_10|L2\.SMC|L3\.TB6GHz_25|L3\.CLW_25|L3\.TB7GHz_10|L1B|L3\.SSW_25|L2\.CLW|L3\.SMC_25|L3\.SND_10|L3\.SST_25|L3\.PRC_25|L3\.TB10GHz_10|L3\.TB7GHz_25|L2\.SST|L3\.SIC_25|L3\.TB18GHz_10|L3\.SST_10|L3\.TB89GHz_10|L1R|L3\.TB23GHz_25|L3\.TB18GHz_25|L3\.TPW_10|L2\.SND|L3\.TB10GHz_25'}
+    domain_map = {"anon-ftp.ceda.ac.uk": re.compile(r"""esacci|aerosol|biomass|cloud|fire|ghg|
+                  glaciers|ice_sheets_antarctica|ice_sheets_greenland|lakes|land_cover|ocean_colour|
+                  ozone|permafrost|sea_ice|sea_level|sea_state|sea_surface_salinity|snow|
+                  soil_moisture|sst""", re.VERBOSE),
+                  "ftp.remss.com": re.compile(r"""amsr2|amsre|amsrj|ascat|ccmp|gmi|msu|nscat|qscat|
+                  seawinds|smap|ssmi|sst|support|TC-winds|tc_wakes|tmi|vapor|water_cycle|
+                  web\.config|welcome\.txt|wind|windsat""", re.VERBOSE),
+                  "ftp.gportal.jaxa.jp": re.compile(r"""JERS-1|ADEOS-2|MOS-1b|SLATS|CIRC|MOS-1|AQUA|
+                  GCOM-C|GCOM-W|GSMaP|ADEOS|GPM|GPMConstellation|TRMM|TRMM_GPMFormat|GCOM-W\.AMSR2|
+                  L3\.PRC_10|L3\.SIC_10|L2\.TPW|L3\.TPW_25|L3\.CLW_10|L3\.SSW_10|L3\.TB36GHz_25|
+                  L3\.TB6GHz_10|L2\.SIC|L3\.TB89GHz_25|L2\.SSW|L3\.TB36GHz_10|L2\.PRC|L3\.SND_25|
+                  L3\.SMC_10|L3\.TB23GHz_10|L2\.SMC|L3\.TB6GHz_25|L3\.CLW_25|L3\.TB7GHz_10|L1B|
+                  L3\.SSW_25|L2\.CLW|L3\.SMC_25|L3\.SND_10|L3\.SST_25|L3\.PRC_25|L3\.TB10GHz_10|
+                  L3\.TB7GHz_25|L2\.SST|L3\.SIC_25|L3\.TB18GHz_10|L3\.SST_10|L3\.TB89GHz_10|L1R|
+                  L3\.TB23GHz_25|L3\.TB18GHz_25|L3\.TPW_10|L2\.SND|L3\.TB10GHz_25""", re.VERBOSE)}
 
     def match_domain(self, raw_attributes):
         """ Find the domain in raw_attributes and set "domain_name" variable.
               Return a string that determines which hardcoded values are being used
               in the following functions """
-        #domain_name = None
-        if set([self.metadata_name]).issubset(raw_attributes.keys()):
-            domain_set = {
-                'ftp.remss.com',
-                'anon-ftp.ceda.ac.uk',
-                'ftp.gportal.jaxa.jp',
-            }
-            if raw_attributes['ftp_domain_name'] in domain_set:
+        metadata_name = 'ftp_domain_name'
+        if set([metadata_name]).issubset(raw_attributes.keys()):
+            if raw_attributes['ftp_domain_name'] in self.domain_map.keys():
                 return raw_attributes['ftp_domain_name']
 
     def dictionary_key_finder(self, raw_attributes, associated_dictionary):
         """
-        Find the correct from the string of 'folder and file name' that are present in the
+        Find the correct part from the string of 'folder and file name' that are present in the
         associated_dictionary. The associated_dictionary varies based on different "get_" functions.
         """
-        pattern = re.compile(self.domain_map[raw_attributes['ftp_domain_name']])
+        pattern = self.domain_map[raw_attributes['ftp_domain_name']]  # , re.VERBOSE)
         return [x for x in pattern.findall(str(raw_attributes['ftp_add_and_file_name'].split('/'))) if x in associated_dictionary.keys()][0]
 
     def get_platform(self, raw_attributes):
@@ -55,25 +61,36 @@ class FTPMetadataNormalizer(BaseMetadataNormalizer):
         dict_for_get_gcmd_platform = {"esacci": 'Earth Observation Satellites',  # ceda
                                       "gmi": 'GPM',  # remss
                                       "GCOM-W": 'GCOM-W1'}  # jaxa
-        if self.match_domain(raw_attributes) is None:
-            return self.match_domain(raw_attributes)
+        match_result = self.match_domain(raw_attributes)
+        if match_result is None:
+            return match_result
         dictionary_key = self.dictionary_key_finder(raw_attributes, dict_for_get_gcmd_platform)
         return pti.get_gcmd_platform(dict_for_get_gcmd_platform[dictionary_key])
 
     def get_instrument(self, raw_attributes):
         """return the corresponding instrument based on specified ftp source """
-        dict_for_get_gcmd_instrument = {"esacci": 'Imaging Spectrometers/Radiometers',  # ceda
-                                        "gmi": 'GMI',  # remss
-                                        "GCOM-W": 'AMSR2'}  # jaxa
-        if self.match_domain(raw_attributes) is None:
-            return self.match_domain(raw_attributes)
-        dictionary_key = self.dictionary_key_finder(raw_attributes, dict_for_get_gcmd_instrument)
-        return pti.get_gcmd_instrument(dict_for_get_gcmd_instrument[dictionary_key])
+        # dict_for_get_gcmd_instrument = {"esacci": 'Imaging Spectrometers/Radiometers',  # ceda
+        #                                "gmi": 'GMI',  # remss
+        #                                "GCOM-W": 'AMSR2'}  # jaxa
+        #match_result = self.match_domain(raw_attributes)
+        # if match_result is None:
+        #    return match_result
+        #dictionary_key = self.dictionary_key_finder(raw_attributes, dict_for_get_gcmd_instrument)
+        # return pti.get_gcmd_instrument(dict_for_get_gcmd_instrument[dictionary_key])
+        instrument_on_ftp = {
+            'ftp://ftp.remss.com/gmi/': 'gmi',
+            'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2': 'AMSR2',
+            'ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/data/CDR_v2/Climatology/L4/v2.1':'Imaging Spectrometers/Radiometers'
+        }
+        for ftp_base_dir in instrument_on_ftp.keys():
+            if ftp_base_dir in 'ftp://'+raw_attributes['ftp_domain_name'].rstrip('/')+'/'+raw_attributes['ftp_add_and_file_name'].lstrip('/'):
+                return pti.get_gcmd_instrument(instrument_on_ftp[ftp_base_dir])
 
     def get_time_coverage_start(self, raw_attributes):
         """ returns the suitable time_coverage_start based on the filename """
-        if self.match_domain(raw_attributes):
-            if self.match_domain(raw_attributes) == "ftp.remss.com":
+        match_result = self.match_domain(raw_attributes)
+        if match_result:
+            if match_result == "ftp.remss.com":
                 extracted_date = raw_attributes['ftp_add_and_file_name'].split(
                     '/')[-1].split('v')[0].split('_')[1]
                 if len(extracted_date) == 6 and extracted_date.startswith('2'):
@@ -90,9 +107,9 @@ class FTPMetadataNormalizer(BaseMetadataNormalizer):
                 else:  # normal cases that are neither being 'd3d' nor inside "week"
                     return dateutil.parser.parse(extracted_date).replace(tzinfo=tzutc())
 
-            elif self.match_domain(raw_attributes) == "anon-ftp.ceda.ac.uk":
+            elif match_result == "anon-ftp.ceda.ac.uk":
                 return dateutil.parser.parse("19820101T000000").replace(tzinfo=tzutc())+relativedelta(days=+int(raw_attributes['ftp_add_and_file_name'].split('/')[-1][1:4]))
-            elif self.match_domain(raw_attributes) == "ftp.gportal.jaxa.jp":
+            elif match_result == "ftp.gportal.jaxa.jp":
                 # time portion of the filename is extracted and then parsed with "dateutil.parser.parse"
                 return dateutil.parser.parse(raw_attributes['ftp_add_and_file_name'].split('/')[-1].split('_')[1]).replace(tzinfo=tzutc())
         else:
@@ -100,8 +117,9 @@ class FTPMetadataNormalizer(BaseMetadataNormalizer):
 
     def get_time_coverage_end(self, raw_attributes):
         """ returns the suitable time_coverage_end based on the filename """
-        if self.match_domain(raw_attributes):
-            if self.match_domain(raw_attributes) == "ftp.remss.com":
+        match_result = self.match_domain(raw_attributes)
+        if match_result:
+            if match_result == "ftp.remss.com":
                 extracted_date = raw_attributes['ftp_add_and_file_name'].split(
                     '/')[-1].split('v')[0].split('_')[1]
                 if len(extracted_date) == 6 and extracted_date.startswith('2'):
@@ -120,10 +138,10 @@ class FTPMetadataNormalizer(BaseMetadataNormalizer):
                 else:  # normal cases that are neither being 'd3d' nor inside "week"
                     return dateutil.parser.parse(extracted_date).replace(tzinfo=tzutc())
 
-            elif self.match_domain(raw_attributes) == "anon-ftp.ceda.ac.uk":
+            elif match_result == "anon-ftp.ceda.ac.uk":
                 return dateutil.parser.parse("20100101T000000").replace(tzinfo=tzutc())+relativedelta(days=+int(raw_attributes['ftp_add_and_file_name'].split('/')[-1][1:4]))
 
-            elif self.match_domain(raw_attributes) == "ftp.gportal.jaxa.jp":
+            elif match_result == "ftp.gportal.jaxa.jp":
                 # time portion of the filename is extracted and then parsed with "dateutil.parser.parse"
                 return self.get_time_coverage_start(raw_attributes)
                 # return dateutil.parser.parse(raw_attributes['ftp_add_and_file_name'].split('/')[-1].split('_')[1]).replace(tzinfo=tzutc())
@@ -135,8 +153,9 @@ class FTPMetadataNormalizer(BaseMetadataNormalizer):
         provider_map = {"anon-ftp.ceda.ac.uk": 'ESA/CCI',
                         "ftp.remss.com": 'NASA/GSFC/SED/ESD/LA/GPM',
                         "ftp.gportal.jaxa.jp": 'JP/JAXA/EOC'}
-        if self.match_domain(raw_attributes) is None:
-            return self.match_domain(raw_attributes)
+        match_result = self.match_domain(raw_attributes)
+        if match_result is None:
+            return match_result
         return pti.get_gcmd_provider(provider_map[raw_attributes['ftp_domain_name']])
 
     def get_dataset_parameters(self, raw_attributes):
@@ -145,8 +164,8 @@ class FTPMetadataNormalizer(BaseMetadataNormalizer):
                                          "L2.SST": ['sea_surface_temperature'],
                                          "gmi": ['wind_speed', 'atmosphere_mass_content_of_water_vapor',
                                                  'atmosphere_mass_content_of_cloud_liquid_water', 'rainfall_rate'], }
-        domain_str = self.match_domain(raw_attributes)
-        if domain_str is None:
+        match_result = self.match_domain(raw_attributes)
+        if match_result is None:
             return []
         else:
             dictionary_key = self.dictionary_key_finder(
@@ -156,10 +175,10 @@ class FTPMetadataNormalizer(BaseMetadataNormalizer):
     def get_location_geometry(self, raw_attributes):
         """ DANGER!!!!! jaxa remains"""
 
-        domain_str = self.match_domain(raw_attributes)
-        if domain_str is None:
+        match_result = self.match_domain(raw_attributes)
+        if match_result is None:
             return None
-        elif domain_str == "anon-ftp.ceda.ac.uk" or "ftp.remss.com":
+        elif match_result == "anon-ftp.ceda.ac.uk" or "ftp.remss.com":
             return GEOSGeometry(('POLYGON((-180 -90, -180 90, 180 90, 180 -90, -180 -90))'), srid=4326)
         else:
             return GEOSGeometry(('POLYGON((1 1, 1 2, 2 2, 2 1, 1 1))'), srid=4326)
