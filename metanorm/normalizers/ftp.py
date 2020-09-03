1912c6721f9814ea8240e9aa47cf7d1900486644
arash <62345618+aanersc@users.noreply.github.com>
2020-09-03 10:29:49 +0430

    Update metanorm/normalizers/ftp.py
    Co-authored-by: Anton Korosov <korosov@gmail.com>

--- metanorm/normalizers/ftp.py
+++ metanorm/normalizers/ftp.py
@@ -129,7 +129,8 @@ class URLMetadataNormalizer(BaseMetadataNormalizer):
         """ DANGER!!!!! jaxa remains(others must be checked!)"""
         if 'url' in raw_attributes:
             urls_geometry = {'ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/': GEOSGeometry(('POLYGON((-180 -90, -180 90, 180 90, 180 -90, -180 -90))'), srid=4326),
-                             'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2': GEOSGeometry(('POLYGON((1 1, 1 2, 2 2, 2 1, 1 1))'), srid=4326),
+                             'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_10': GEOSGeometry(('POLYGON((-180 -90, -180 90, 180 90, 180 -90, -180 -90))'), srid=4326),
+                             'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_25': GEOSGeometry(('POLYGON((-180 -90, -180 90, 180 90, 180 -90, -180 -90))'), srid=4326),
                              "ftp://ftp.remss.com/gmi/": GEOSGeometry(('POLYGON((-180 -90, -180 90, 180 90, 180 -90, -180 -90))'), srid=4326)}
             return self.get_matching_value(urls_geometry, raw_attributes, GEOSGeometry)
 
