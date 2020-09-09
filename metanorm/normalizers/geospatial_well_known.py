e6175c75b925a28966a401cba90194e957dfb8f7
Anton Korosov <korosov@gmail.com>
2020-09-09 09:57:22 +0200

    #23 remove geometry_from_wkt_string

--- metanorm/normalizers/geospatial_well_known.py
+++ metanorm/normalizers/geospatial_well_known.py
@@ -14,14 +14,13 @@ class GeoSpatialWellKnownMetadataNormalizer(BaseMetadataNormalizer):
         if set(['northernmost_latitude', 'southernmost_latitude',
                 'easternmost_longitude', 'westernmost_longitude']).issubset(raw_attributes.keys()):
 
-            polygon = utils.wkt_polygon_from_wgs84_limits(
+            return utils.wkt_polygon_from_wgs84_limits(
                 raw_attributes['northernmost_latitude'],
                 raw_attributes['southernmost_latitude'],
                 raw_attributes['easternmost_longitude'],
                 raw_attributes['westernmost_longitude']
             )
 
-            return utils.geometry_from_wkt_string(polygon)
         else:
             raise MetadataNormalizationError(
                 "Unable to find a value for the 'geographic_location' parameter")
