be182f31029964c1cc880585e6cf77f157c93504
Adrien Perrin <adrien.perrin@nersc.no>
2020-10-28 10:47:03 +0000

    fix dictionaries indentation in the url normalizer

--- metanorm/normalizers/url.py
+++ metanorm/normalizers/url.py
@@ -18,13 +18,16 @@ LOGGER.addHandler(logging.NullHandler())
 
 class URLMetadataNormalizer(BaseMetadataNormalizer):
     """ Normalizer for hardcoding information based on URLS """
+
     urls_platforms = {
         "ftp://ftp.remss.com/gmi": 'GPM',
         "ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/": 'Earth Observation Satellites',
         "ftp://ftp.gportal.jaxa.jp/standard/GCOM-W": 'GCOM-W1',
-        "ftp://nrt.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046": 'Earth Observation satellites',
+        "ftp://nrt.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046":
+            'Earth Observation satellites',
         "ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003": 'Earth Observation satellites',
-        "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024": 'OPERATIONAL MODELS'}
+        "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024": 'OPERATIONAL MODELS'
+    }
 
     urls_instruments = {
         'ftp://ftp.remss.com/gmi': 'GMI',
@@ -33,7 +36,8 @@ class URLMetadataNormalizer(BaseMetadataNormalizer):
         'Imaging Spectrometers/Radiometers',
         "ftp://nrt.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046": 'altimeters',
         "ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003": 'altimeters',
-        "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024": 'computer'}
+        "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024": 'computer'
+    }
 
     urls_provider = {
         'ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/data/CDR_v2/Climatology/L4/v2.1': 'ESA/CCI',
@@ -41,75 +45,97 @@ class URLMetadataNormalizer(BaseMetadataNormalizer):
         "ftp://ftp.gportal.jaxa.jp/standard": 'JP/JAXA/EOC',
         "ftp://nrt.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046": 'cmems',
         "ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003": 'cmems',
-        "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024": 'cmems'}
+        "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024": 'cmems'
+    }
 
-    keys_for_geometry_dictionary = {'ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/',
-                                    'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_10',
-                                    'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_25',
-                                    "ftp://ftp.remss.com/gmi/",
-                                    "ftp://nrt.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046",
-                                    "ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003",
-                                    "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024"}
+    keys_for_geometry_dictionary = {
+        'ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/',
+        'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_10',
+        'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_25',
+        "ftp://ftp.remss.com/gmi/",
+        "ftp://nrt.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046",
+        "ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003",
+        "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024"
+    }
+
     urls_geometry = dict.fromkeys(keys_for_geometry_dictionary,
                                   ('POLYGON((-180 -90, -180 90, 180 90, 180 -90, -180 -90))'))
 
     urls_title = {
-        'ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/data/CDR_v2/Climatology/L4/v2.1': 'ESA SST CCI OSTIA L4 Climatology',
-        "ftp://ftp.remss.com/gmi/": 'Atmosphere parameters from Global Precipitation Measurement Microwave Imager',
-        "ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L2.SST/": 'AMSR2-L2 Sea Surface Temperature',
-        "ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_10/": 'AMSR2-L2 Sea Surface Temperature',
-        "ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_25/": 'AMSR2-L3 Sea Surface Temperature',
-        "ftp://nrt.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046": 'GLOBAL OCEAN GRIDDED L4 SEA SURFACE HEIGHTS AND DERIVED VARIABLES NRT',
-        "ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003": 'GLOBAL TOTAL SURFACE AND 15M CURRENT FROM ALTIMETRIC GEOSTROPHIC CURRENT AND MODELED EKMAN CURRENT PROCESSING',
-        "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024": 'GLOBAL OCEAN 1_12 PHYSICS ANALYSIS AND FORECAST UPDATED DAILY'
+        'ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/data/CDR_v2/Climatology/L4/v2.1':
+            'ESA SST CCI OSTIA L4 Climatology',
+        "ftp://ftp.remss.com/gmi/":
+            'Atmosphere parameters from Global Precipitation Measurement Microwave Imager',
+        "ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L2.SST/":
+            'AMSR2-L2 Sea Surface Temperature',
+        "ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_10/":
+            'AMSR2-L2 Sea Surface Temperature',
+        "ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_25/":
+            'AMSR2-L3 Sea Surface Temperature',
+        "ftp://nrt.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046":
+            'GLOBAL OCEAN GRIDDED L4 SEA SURFACE HEIGHTS AND DERIVED VARIABLES NRT',
+        "ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003":
+            'GLOBAL TOTAL SURFACE AND 15M CURRENT FROM ALTIMETRIC '
+            'GEOSTROPHIC CURRENT AND MODELED EKMAN CURRENT PROCESSING',
+        "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024":
+            'GLOBAL OCEAN 1_12 PHYSICS ANALYSIS AND FORECAST UPDATED DAILY'
     }
 
-    urls_entry_id = {"https://thredds.met.no/thredds/dodsC/osisaf/met.no/ice":
-                     re.compile(r"([^/]+)\.nc\.dods$"),
-                     "https://opendap.jpl.nasa.gov/opendap/": re.compile(r"([^/]+)\.(nc|h5)(\.gz)?$"),
-                     "ftp://ftp.remss.com/gmi": re.compile(r"([^/]+)\.gz$"),
-                     "ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/":
-                     re.compile(r"([^/]+)\.(nc|h5)(\.gz)?$"),
-                     "ftp://nrt.cmems-du.eu/Core/":
-                     re.compile(r"([^/]+)\.(nc|h5)(\.gz)?$"),
-                     "ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/data/CDR_v2/Climatology/":
-                     re.compile(r"([^/]+)\.(nc|h5)(\.gz)?$")
-                     }
+    urls_entry_id = {
+        "https://thredds.met.no/thredds/dodsC/osisaf/met.no/ice": re.compile(r"([^/]+)\.nc\.dods$"),
+        "https://opendap.jpl.nasa.gov/opendap/": re.compile(r"([^/]+)\.(nc|h5)(\.gz)?$"),
+        "ftp://ftp.remss.com/gmi": re.compile(r"([^/]+)\.gz$"),
+        "ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/":
+            re.compile(r"([^/]+)\.(nc|h5)(\.gz)?$"),
+        "ftp://nrt.cmems-du.eu/Core/": re.compile(r"([^/]+)\.(nc|h5)(\.gz)?$"),
+        "ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/data/CDR_v2/Climatology/":
+            re.compile(r"([^/]+)\.(nc|h5)(\.gz)?$")
+    }
 
-    urls_dsp = {'ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/': ['sea_surface_temperature'],
-                'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L2.SST':
-                ['sea_surface_temperature'],
-                'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_10':
-                ['sea_surface_temperature'],
-                'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_25':
-                ['sea_surface_temperature'],
-                "ftp://ftp.remss.com/gmi/":
-                ['wind_speed', 'atmosphere_mass_content_of_water_vapor',
-                 'atmosphere_mass_content_of_cloud_liquid_water', 'rainfall_rate'],
-                "ftp://nrt.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046":
-                # based on http://nrt.cmems-du.eu/motu-web/Motu?action=describeProduct&service=SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046-TDS&product=dataset-duacs-nrt-global-merged-allsat-phy-l4
-                ['surface_geostrophic_eastward_sea_water_velocity',
-                 'surface_geostrophic_eastward_sea_water_velocity_assuming_sea_level_for_geoid',
-                 'surface_geostrophic_northward_sea_water_velocity',
-                 'surface_geostrophic_northward_sea_water_velocity_assuming_sea_level_for_geoid'],
-                "ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003":
-                # based on http://nrt.cmems-du.eu/motu-web/Motu?action=describeProduct&service=MULTIOBS_GLO_PHY_NRT_015_003-TDS&product=dataset-uv-nrt-daily
-                ['eastward_sea_water_velocity',
-                 'northward_sea_water_velocity'],
-                "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024":
-                # based on http://nrt.cmems-du.eu/motu-web/Motu?action=describeProduct&service=GLOBAL_ANALYSIS_FORECAST_PHY_001_024-TDS&product=global-analysis-forecast-phy-001-024
-                # ['sea_water_potential_temperature_at_sea_floor', # problematic for pti!! DANGER TODO
-                ['ocean_mixed_layer_thickness_defined_by_sigma_theta',
-                 'sea_ice_area_fraction',
-                 'sea_ice_thickness',
-                 'sea_water_salinity',
-                 'sea_water_potential_temperature',
-                 'eastward_sea_water_velocity',
-                 'eastward_sea_ice_velocity',
-                 'northward_sea_water_velocity',
-                 'northward_sea_ice_velocity',
-                 'sea_surface_height_above_geoid']
-                }
+    urls_dsp = {
+        'ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/': ['sea_surface_temperature'],
+        'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L2.SST': [
+            'sea_surface_temperature'
+        ],
+        'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_10': [
+            'sea_surface_temperature'
+        ],
+        'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2/L3.SST_25': [
+            'sea_surface_temperature'
+        ],
+        "ftp://ftp.remss.com/gmi/": [
+            'wind_speed',
+            'atmosphere_mass_content_of_water_vapor',
+            'atmosphere_mass_content_of_cloud_liquid_water',
+            'rainfall_rate'
+        ],
+        # based on http://nrt.cmems-du.eu/motu-web/Motu?action=describeProduct&service=SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046-TDS&product=dataset-duacs-nrt-global-merged-allsat-phy-l4
+        "ftp://nrt.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046": [
+            'surface_geostrophic_eastward_sea_water_velocity',
+            'surface_geostrophic_eastward_sea_water_velocity_assuming_sea_level_for_geoid',
+            'surface_geostrophic_northward_sea_water_velocity',
+            'surface_geostrophic_northward_sea_water_velocity_assuming_sea_level_for_geoid'
+        ],
+        # based on http://nrt.cmems-du.eu/motu-web/Motu?action=describeProduct&service=MULTIOBS_GLO_PHY_NRT_015_003-TDS&product=dataset-uv-nrt-daily
+        "ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003": [
+            'eastward_sea_water_velocity',
+            'northward_sea_water_velocity'
+        ],
+        # based on http://nrt.cmems-du.eu/motu-web/Motu?action=describeProduct&service=GLOBAL_ANALYSIS_FORECAST_PHY_001_024-TDS&product=global-analysis-forecast-phy-001-024
+        "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024": [
+            # 'sea_water_potential_temperature_at_sea_floor', # problematic for pti!! DANGER TODO
+            'ocean_mixed_layer_thickness_defined_by_sigma_theta',
+            'sea_ice_area_fraction',
+            'sea_ice_thickness',
+            'sea_water_salinity',
+            'sea_water_potential_temperature',
+            'eastward_sea_water_velocity',
+            'eastward_sea_ice_velocity',
+            'northward_sea_water_velocity',
+            'northward_sea_ice_velocity',
+            'sea_surface_height_above_geoid'
+        ]
+    }
 
     @staticmethod
     def find_matching_value(associated_dict, raw_attributes):
@@ -203,29 +229,34 @@ class URLMetadataNormalizer(BaseMetadataNormalizer):
             # Sometimes there is constant value needed for this calculation instead of filename
             url_time = {
                 'ftp://ftp.remss.com': file_name,
-                'ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/data/CDR_v2/Climatology/L4/v2.1': "19820101" if start else "20100101",
+                'ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/data/CDR_v2/Climatology/L4/v2.1':
+                    "19820101" if start else "20100101",
                 "ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2":
-                file_name_splitted[0] + '_' + file_name_splitted[1] if file_name_splitted else None,
+                    file_name_splitted[0] + '_' +
+                    file_name_splitted[1] if file_name_splitted else None,
                 "ftp://nrt.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046/dataset-duacs-nrt-global-merged-allsat-phy-l4":
-                file_name[:33],
+                    file_name[:33],
                 "ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003":
-                file_name_splitted[0] + '_' + file_name_splitted[1] if file_name_splitted else None,
+                    file_name_splitted[0] + '_' +
+                    file_name_splitted[1] if file_name_splitted else None,
                 "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024/global-analysis-forecast-phy-001-024/":
-                file_name_splitted[0] + '_' +
+                    file_name_splitted[0] + '_' +
                     file_name_splitted[-2] if file_name_splitted else None,
                 "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024/global-analysis-forecast-phy-001-024-3dinst-so/":
-                file_name[:36],
+                    file_name[:36],
                 "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024/global-analysis-forecast-phy-001-024-3dinst-thetao":
-                file_name[:40],
+                    file_name[:40],
                 "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024/global-analysis-forecast-phy-001-024-3dinst-uovo":
-                file_name[:38],
+                    file_name[:38],
                 "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024/global-analysis-forecast-phy-001-024-hourly-merged-uv":
-                file_name_splitted[0] + '_' + file_name_splitted[1] if file_name_splitted else None,
+                    file_name_splitted[0] + '_' +
+                file_name_splitted[1] if file_name_splitted else None,
                 "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024/global-analysis-forecast-phy-001-024-hourly-t-u-v-ssh/":
-                file_name_splitted[0] + '_' +
+                    file_name_splitted[0] + '_' +
                     file_name_splitted[-2] if file_name_splitted else None,
                 "ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024/global-analysis-forecast-phy-001-024-monthly/":
-                file_name_splitted[0] + '_' + file_name_splitted[-1] if file_name_splitted else None
+                    file_name_splitted[0] + '_' +
+                    file_name_splitted[-1] if file_name_splitted else None
             }
             extracted_date = self.extract_time(self.find_matching_value(
                 url_time, raw_attributes))
@@ -250,24 +281,28 @@ class URLMetadataNormalizer(BaseMetadataNormalizer):
                     # year is added.
                     extracted_date = extracted_date if start else \
                         extracted_date + self.length_of_month(extracted_date)
-            elif raw_attributes['url'].startswith('ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/data/CDR_v2/Climatology/L4/v2.1'):
+            elif raw_attributes['url'].startswith(
+                    'ftp://anon-ftp.ceda.ac.uk/neodc/esacci/sst/data/CDR_v2/Climatology/L4/v2.1'):
                 # the constant date is corrected based on the few letter at the beginning of file name
                 extracted_date += relativedelta(days=int(file_name[1:4]) - 1)
-            elif raw_attributes['url'].startswith('ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2'):
+            elif raw_attributes['url'].startswith(
+                    'ftp://ftp.gportal.jaxa.jp/standard/GCOM-W/GCOM-W.AMSR2'):
                 if file_name_splitted[1].endswith('00'):  # it is a month file
                     extracted_date = extracted_date if start else \
                         extracted_date + self.length_of_month(extracted_date)
             elif raw_attributes['url'].startswith(
                     "ftp://nrt.cmems-du.eu/Core/SEALEVEL_GLO_PHY_L4_NRT_OBSERVATIONS_008_046"):
                 extracted_date = extracted_date if start else extracted_date + relativedelta(days=1)
-            elif raw_attributes['url'].startswith('ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003'):
+            elif raw_attributes['url'].startswith(
+                    'ftp://nrt.cmems-du.eu/Core/MULTIOBS_GLO_PHY_NRT_015_003'):
                 if "monthly" in file_name:
                     extracted_date = extracted_date if start else \
                         extracted_date + self.length_of_month(extracted_date)
                 if "hourly" in file_name or "daily" in file_name:
                     extracted_date = extracted_date if start else \
                         extracted_date + relativedelta(days=1)
-            elif raw_attributes['url'].startswith('ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024'):
+            elif raw_attributes['url'].startswith(
+                    'ftp://nrt.cmems-du.eu/Core/GLOBAL_ANALYSIS_FORECAST_PHY_001_024'):
                 if any(path_part.endswith("monthly") for path_part in url_path_and_file_name_splitted):
                     extracted_date = extracted_date if start else \
                         extracted_date + self.length_of_month(extracted_date)
