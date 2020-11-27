d9b708c3d6fbcb7328cff3b72fd574cbaeb3bcf4
Adrien Perrin <adrien.perrin@nersc.no>
2020-11-27 13:44:15 +0000

    add tests for time utils

---
+++ tests/test_utils.py
@@ -0,0 +1,56 @@
+import re
+import unittest
+from datetime import datetime
+
+from dateutil.tz import tzutc
+
+import metanorm.utils as utils
+
+class TimeTestCase(unittest.TestCase):
+    """Tests for utilities dealing with time"""
+
+    def test_create_datetime_year_month_day(self):
+        """test create_datetime with a year, month and day"""
+        self.assertEqual(
+            utils.create_datetime(2020, 10, 15),
+            datetime(2020, 10, 15).replace(tzinfo=tzutc())
+        )
+
+    def test_create_datetime_year_day_of_year(self):
+        """test create_datetime with a year and day of year"""
+        self.assertEqual(
+            utils.create_datetime(2020, day_of_year=35),
+            datetime(2020, 2, 4).replace(tzinfo=tzutc())
+        )
+
+    def test_create_datetime_year_month_day_time(self):
+        """test create_datetime with a year, month, day and time"""
+        self.assertEqual(
+            utils.create_datetime(2020, 10, 15, hour=10, minute=25, second=38),
+            datetime(2020, 10, 15, 10, 25, 38).replace(tzinfo=tzutc())
+        )
+
+    def test_create_datetime_year_day_of_year_time(self):
+        """test create_datetime with a year, day of year and time"""
+        self.assertEqual(
+            utils.create_datetime(2020, day_of_year=35, hour=23, minute=1, second=40),
+            datetime(2020, 2, 4, 23, 1, 40).replace(tzinfo=tzutc())
+        )
+
+    def test_yearmonth_regex(self):
+        """The YEARMONTH_REGEX should provide a 'year' and 'month'
+        named groups
+        """
+        self.assertDictEqual(
+            re.match(utils.YEARMONTH_REGEX, '202010').groupdict(),
+            {'year': '2020', 'month': '10'}
+        )
+
+    def test_yearmonthday_regex(self):
+        """The YEARMONTHDAY_REGEX should provide a 'year', 'month'
+        and 'day' named groups
+        """
+        self.assertDictEqual(
+            re.match(utils.YEARMONTHDAY_REGEX, '20201017').groupdict(),
+            {'year': '2020', 'month': '10', 'day': '17'}
+        )
