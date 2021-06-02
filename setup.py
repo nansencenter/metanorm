7099f6bb8554be7d91d41e2973ad2c6c45f99cde
Adrien Perrin <adrien.perrin@nersc.no>
2021-06-02 13:49:58 +0200

    update setup.py version to 3.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="metanorm",
-    version="3.0.0",
+    version="3.1.0",
     author="Adrien Perrin, Arash Azamifard",
     author_email="adrien.perrin@nersc.no",
     description="Normalizing tool for Geospatial metadata",
