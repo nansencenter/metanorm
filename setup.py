b1591e9cde9ac19be3b37ff8d0febf15b86606e7
arash <62345618+azamifarda@users.noreply.github.com>
2021-06-08 09:15:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="metanorm",
-    version="3.1.0",
+    version="3.1.1",
     author="Adrien Perrin, Arash Azamifard",
     author_email="adrien.perrin@nersc.no",
     description="Normalizing tool for Geospatial metadata",
