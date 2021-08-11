b0c2a0b7dda0db011586db5ab5216ea0ca5bb226
Adrien Perrin <adrien.perrin@nersc.no>
2021-08-11 07:33:12 +0000

    refacto: add new base normalizer class

--- metanorm/normalizers/base.py
+++ metanorm/normalizers/base.py
@@ -7,6 +7,20 @@ from metanorm.errors import MetadataNormalizationError
 LOGGER = logging.getLogger(__name__)
 LOGGER.addHandler(logging.NullHandler())
 
+class MetadataNormalizer():
+    """Base class for all metadata normalizers"""
+
+    def check(self, raw_metadata):
+        """Returns a boolean indicating whether the normalizer is
+        capable of handling the raw metadata.
+        Only leaf classes should return True
+        """
+        return False
+
+    def normalize(self, raw_metadata):
+        """Normalizes the raw metadata. Should return a dictionary"""
+        raise NotImplementedError
+
 
 class BaseMetadataNormalizer():
     """Base class for standard normalizers"""
