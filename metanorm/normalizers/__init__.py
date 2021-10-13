7a61c777b5d71f75c45ac0c5bc5ef2fb3c9a673d
Adrien Perrin <adrien.perrin@nersc.no>
2021-10-13 14:49:07 +0000

    remove old normalizers structure

--- metanorm/normalizers/__init__.py
+++ metanorm/normalizers/__init__.py
@@ -5,9 +5,8 @@ and don't forget to write tests!
 """
 import os.path
 
-from .base import BaseMetadataNormalizer, MetadataNormalizer
+from .base import MetadataNormalizer
 from ..utils import export_subclasses
 
 __all__ = []
 export_subclasses(__all__, __package__, os.path.dirname(__file__), MetadataNormalizer)
-export_subclasses(__all__, __package__, os.path.dirname(__file__), BaseMetadataNormalizer)
