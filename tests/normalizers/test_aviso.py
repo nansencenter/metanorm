bccb6be39d102116070edaa0fe37856acb3aaa42
Adrien Perrin <adrien.perrin@nersc.no>
2022-11-17 09:47:44 +0000

    update aviso tests

--- tests/normalizers/test_aviso.py
+++ tests/normalizers/test_aviso.py
@@ -2,7 +2,6 @@
 
 import unittest
 import unittest.mock as mock
-from collections import OrderedDict
 from datetime import datetime, timezone
 
 import metanorm.normalizers as normalizers
@@ -134,29 +133,26 @@ class AVISOAltimetryMetadataNormalizerTests(unittest.TestCase):
         with self.assertRaises(MetadataNormalizationError):
             self.normalizer.get_time_coverage_end({})
 
-    def test_get_platform(self):
+    def test_gcmd_platform(self):
         """Test getting the platform"""
-        self.assertDictEqual(
-            self.normalizer.get_platform({}),
-            OrderedDict([
-                ('Category', 'Earth Observation Satellites'),
-                ('Series_Entity', ''),
-                ('Short_Name', ''),
-                ('Long_Name', '')])
-        )
+        with mock.patch('metanorm.utils.get_gcmd_platform') as mock_get_gcmd_method:
+            self.assertEqual(
+                self.normalizer.get_platform({}),
+                mock_get_gcmd_method.return_value)
 
-    def test_get_instrument(self):
+    def test_gcmd_instrument(self):
         """Test getting the instrument"""
-        self.assertDictEqual(
-            self.normalizer.get_instrument({}),
-            OrderedDict([
-                ('Category', 'Earth Remote Sensing Instruments'),
-                ('Class', 'Active Remote Sensing'),
-                ('Type', 'Altimeters'),
-                ('Subtype', ''),
-                ('Short_Name', ''),
-                ('Long_Name', '')])
-        )
+        with mock.patch('metanorm.utils.get_gcmd_instrument') as mock_get_gcmd_method:
+            self.assertEqual(
+                self.normalizer.get_instrument({}),
+                mock_get_gcmd_method.return_value)
+
+    def test_gcmd_provider(self):
+        """Test getting the provider"""
+        with mock.patch('metanorm.utils.get_gcmd_provider') as mock_get_gcmd_method:
+            self.assertEqual(
+                self.normalizer.get_provider({}),
+                mock_get_gcmd_method.return_value)
 
     def test_get_location_geometry(self):
         """Test getting the geometry"""
@@ -168,17 +164,3 @@ class AVISOAltimetryMetadataNormalizerTests(unittest.TestCase):
         """
         self.assertEqual(self.normalizer.get_location_geometry({}), '')
 
-    def test_get_provider(self):
-        """Test getting the provider"""
-        self.assertDictEqual(
-            self.normalizer.get_provider({}),
-            OrderedDict([
-                ('Bucket_Level0', 'COMMERCIAL'),
-                ('Bucket_Level1', ''),
-                ('Bucket_Level2', ''),
-                ('Bucket_Level3', ''),
-                ('Short_Name', 'AVISO'),
-                ('Long_Name',
-                'Archiving, Validation and Interpretation of Satellite Oceanographic Data'),
-                ('Data_Center_URL', 'http://www.aviso.oceanobs.com/')])
-        )
